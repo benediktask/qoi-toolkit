fd463dfcb70d186f13208eda8da8c64d919ece4d
Benediktas <4579078+benediktask@users.noreply.github.com>
2026-05-29 10:30:13 +0300

    add run-length encoding

--- src/encode.ts
+++ src/encode.ts
@@ -1,5 +1,6 @@
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
+import { hrtime } from 'node:process';
 
 // sample.bin
 // const FILENAME = 'sample';
@@ -7,15 +8,17 @@ import * as path from 'node:path';
 // const IMAGE_HEIGHT = 588;
 // const IMAGE_CHANNEL_COUNT = 4;
 
-// cat.bin
-const FILENAME = 'cat';
-const IMAGE_WIDTH = 3000;
-const IMAGE_HEIGHT = 4000;
-const IMAGE_CHANNEL_COUNT = 4;
+function finalizeRun(output: Uint8Array[], trackers: { runLength: number }, condition: (runLength: number) => boolean) {
+    if (condition(trackers.runLength) === true) {
+        output.push(new Uint8Array([0b11000000 | (trackers.runLength - 1)]));
+        trackers.runLength = 0;
+    }
+}
 
 async function encode() {
+    const start = process.hrtime.bigint();
     const rawPixelsBuffer = await fs.readFile(path.join(import.meta.dirname, '..', 'assets', `${FILENAME}.bin`));
-    const rawPixelsBufferCopy = new Uint8Array(rawPixelsBuffer.buffer, rawPixelsBuffer.byteOffset, rawPixelsBuffer.byteLength).slice();
+    const rawPixelsBufferCopy = new Uint8Array(rawPixelsBuffer.buffer, rawPixelsBuffer.byteOffset, rawPixelsBuffer.byteLength);
 
     const outputChunks = [
         // magic
@@ -43,19 +46,36 @@ async function encode() {
         ]),
     ];
 
+    let trackers = {
+        runLength: 0,
+    };
+
+    let prevPixelRed = 0;
+    let prevPixelGreen = 0;
+    let prevPixelBlue = 0;
+    let prevPixelAlpha = 255;
+
     for (let offset = 0; offset < rawPixelsBufferCopy.byteLength - IMAGE_CHANNEL_COUNT; offset += IMAGE_CHANNEL_COUNT) {
-        const rawPixel = rawPixelsBufferCopy.slice(offset, offset + IMAGE_CHANNEL_COUNT);
+        const [r, g, b, a] = rawPixelsBufferCopy.subarray(offset, offset + IMAGE_CHANNEL_COUNT);
 
-        const qoiRgbaOpChunk = new Uint8Array([
-            0b11111111, // tag
-            rawPixel[0], // r
-            rawPixel[1], // g
-            rawPixel[2], // b
-            rawPixel[3], // a
-        ]);
+        if (r === prevPixelRed && g === prevPixelGreen && b === prevPixelBlue && a === prevPixelAlpha) {
+            trackers.runLength++;
 
-        outputChunks.push(qoiRgbaOpChunk);
+            finalizeRun(outputChunks, trackers, runLength => runLength === 62);
+        } else {
+            finalizeRun(outputChunks, trackers, runLength => runLength > 0);
+
+            const QOI_RGBA_OP_CHUNK = new Uint8Array([0b11111111, r, g, b, a]);
+            outputChunks.push(QOI_RGBA_OP_CHUNK);
+        }
+
+        prevPixelRed = r;
+        prevPixelGreen = g;
+        prevPixelBlue = b;
+        prevPixelAlpha = a;
     }
+
+    finalizeRun(outputChunks, trackers, runLength => runLength > 0);
 
     // eof mark
     outputChunks.push(new Uint8Array([
@@ -70,6 +90,7 @@ async function encode() {
     ]));
 
     await fs.writeFile(path.join(import.meta.dirname, '..', 'assets', `${FILENAME}.qoi`), Buffer.concat(outputChunks));
+    console.log(`Process took: ${(process.hrtime.bigint() - start) / 1000n / 1000n} ms`);
 }
 
 await encode();
